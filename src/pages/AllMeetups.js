accac451f617979343d92aaf42fc1c62d72275e3
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-05-06 01:03:47 +0300

    outputting lists

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,7 +1,35 @@
 import React from "react";
 
+const DUMMY_DATA = [
+    {
+        id: "m1",
+        title: "First meetup",
+        address: "Lelas Karagianni 42",
+        description: "my first meetup description",
+        image:
+            "https://cdn.pixabay.com/photo/2020/07/08/04/12/work-5382501_960_720.jpg",
+    },
+    {
+        id: "m2",
+        title: "Second meetup",
+        address: "Andrea Metaxa 15",
+        description: "my amazing second meetup description",
+        image:
+            "https://cdn.pixabay.com/photo/2015/01/08/18/27/startup-593341_960_720.jpg",
+    },
+];
+
 const AllMeetupsPage = () => {
-    return <div>All Meetups Page</div>;
+    return (
+        <section>
+            <h1>All Meetups Page</h1>
+            <ul>
+                {DUMMY_DATA.map((meetup) => {
+                    return <li key={meetup.id}>{meetup.title}</li>;
+                })}
+            </ul>
+        </section>
+    );
 };
 
 export default AllMeetupsPage;
