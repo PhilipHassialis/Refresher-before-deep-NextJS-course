88ba3495c2c7453879b0dbc9da4e43ed50677f1e
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-05-09 11:03:39 +0300

    updating state based on previous state

--- src/store/favourites-context.js
+++ src/store/favourites-context.js
@@ -8,6 +8,22 @@ const FavouritesContext = createContext({
 const FavouritesContextProvider = ({ chilren }) => {
     const [userFavourites, setUserFavourites] = useState([]);
 
+    const addFavouriteHandler = (favouriteMeetup) => {
+        setUserFavourites((prevState) => {
+            return prevState.concat(favouriteMeetup);
+        });
+    };
+
+    const removeFavouriteHandler = (meetupId) => {
+        setUserFavourites((prevState) => {
+            return prevState.filter((meetup) => meetup.id !== meetupId);
+        });
+    };
+
+    const itemIsFavouriteHandler = (meetupId) => {
+        return userFavourites.some((meetup) => meetup.id === meetupId);
+    };
+
     const context = {
         favourites: userFavourites,
         totalFavourites: userFavourites.length,
