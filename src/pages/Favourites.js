c7da1f83b63666a300873590e56380b3c7f8d2ef
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-05-09 11:31:00 +0300

    More context usage

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import FavouritesContext from "../store/favourites-context";
+import MeetupList from "../components/meetups/MeetupList";
 
 const FavouritesPage = () => {
-    return <div>Favourites Page</div>;
+    const favouritesCtx = useContext(FavouritesContext);
+
+    let content;
+    if (favouritesCtx.totalFavourites === 0)
+        content = <p>You got no favourites yet</p>;
+    else content = <MeetupList meetups={favouritesCtx.favourites}></MeetupList>;
+
+    return (
+        <section>
+            <h1>My Favourites</h1>
+            {content}
+        </section>
+    );
 };
 
 export default FavouritesPage;
